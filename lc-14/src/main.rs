84a8a356382f72e3c89818e0420bf8269d544a6a
Moritz Konarski <moritz.konarski@gmail.com>
2023-12-07 08:10:48 +0100

    feat: finish problem

--- lc-14/src/main.rs
+++ lc-14/src/main.rs
@@ -2,45 +2,84 @@ use std::collections::HashMap;
 
 impl Solution {
     pub fn longest_common_prefix(strs: Vec<String>) -> String {
-        // this is ok because only lowercase letters are used
         if strs.len() == 1 {
             return strs[0].to_owned();
         }
 
-        let mut root = TreeNode::new(0);
-
+        let mut root = TreeNode::new(37);
         for mut s in strs {
             if s.is_empty() {
                 return "".to_string();
             }
+
             let mut node = &mut root;
             s.push('$');
             for byte in s.as_bytes() {
                 node = node.get_or_add_child(byte);
             }
         }
-        // println!("{:?}", root);
+        println!("{:?}", root);
+
+        if root.children.len() != 1 {
+            return "".to_string();
+        }
 
         let mut letters = Vec::new();
-
-        let mut node = &mut root;
+        let mut key = root.children.keys().next().unwrap().to_owned();
+        let mut node = root.children.get_mut(&key).unwrap();
         while node.children.len() == 1 {
             letters.push(node.letter);
-            let key = node.children.keys().next().unwrap().to_owned();
+            key = node.children.keys().next().unwrap().to_owned();
             node = node.children.get_mut(&key).unwrap();
         }
-        letters.push(node.letter);
-
-        if letters.len() == 1 {
-            "".to_string()
-        } else {
-            letters.remove(0);
-            String::from_utf8(letters).unwrap()
+        if node.letter != 36 {
+            letters.push(node.letter);
         }
+
+        String::from_utf8(letters).unwrap()
+    }
+
+    /// very fast at 0 ms but with 2.18 MB
+    pub fn longest_common_prefix1(strs: Vec<String>) -> String {
+        if strs.len() == 1 {
+            return strs[0].to_owned();
+        }
+
+        let mut root = TreeNode::new(37);
+        for mut s in strs {
+            if s.is_empty() {
+                return "".to_string();
+            }
+
+            let mut node = &mut root;
+            s.push('$');
+            for byte in s.as_bytes() {
+                node = node.get_or_add_child(byte);
+            }
+        }
+        println!("{:?}", root);
+
+        if root.children.len() != 1 {
+            return "".to_string();
+        }
+
+        let mut letters = Vec::new();
+        let mut key = root.children.keys().next().unwrap().to_owned();
+        let mut node = root.children.get_mut(&key).unwrap();
+        while node.children.len() == 1 {
+            letters.push(node.letter);
+            key = node.children.keys().next().unwrap().to_owned();
+            node = node.children.get_mut(&key).unwrap();
+        }
+        if node.letter != 36 {
+            letters.push(node.letter);
+        }
+
+        String::from_utf8(letters).unwrap()
     }
 }
 
-// #[derive(Debug)]
+#[derive(Debug)]
 struct TreeNode {
     pub letter: u8,
     pub children: HashMap<u8, Self>,
@@ -84,6 +123,17 @@ fn ex_3() {
 fn ex_4() {
     let strs = vec!["ab".into(), "a".into()];
     assert_eq!("a".to_string(), Solution::longest_common_prefix(strs));
+}
+
+#[test]
+fn ex_5() {
+    let strs = vec![
+        "flower".into(),
+        "flower".into(),
+        "flower".into(),
+        "flower".into(),
+    ];
+    assert_eq!("flower".to_string(), Solution::longest_common_prefix(strs));
 }
 
 struct Solution {}
